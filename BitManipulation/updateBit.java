bcca61ba29e9719b4dbe178391931c1604795ec4
Mohammad Tofik <mohammad.tofik@wdc.com>
2023-01-25 23:34:17 +0530

    First Commit

---
+++ BitManipulation/updateBit.java
@@ -0,0 +1,20 @@
+/** We used updateBit to update the bit on any position in a given binary number. */
+
+/**
+ * Idea : 
+ * For 1
+ * bitMask : 1 << position
+ * Operation : AND with NOT
+ */
+
+ /**
+  * For 0
+  *bitMask : 1 << position
+  *Operation : OR
+  */
+public class updateBit {
+    public static void main(String[] args) {
+        
+    }
+    
+}
